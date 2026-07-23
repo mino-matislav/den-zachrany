c640a7e8a6ed894fff6788e995f5786ebfec9cef
mino-matislav <mino.matislav@gmail.com>
2026-07-23 13:04:37 +0200

    Initial commit - MVP

---
+++ js/data-kapitoly.js
@@ -0,0 +1,145 @@
+const chapters = [
+  {
+    id: "1",
+    title: "Strach zo smrti a večného zatratenia",
+    shortTitle: "Strach zo smrti",
+    subtitle: "Ako skrze Kristovu milosť prijať dar neotrasiteľného spasenia",
+    shortDescription: "Znovuzrodenie ako štartovací bod. Ospravedlnenie z milosti a moc Ducha Svätého.",
+    isStarter: true,
+    tags: ["strach zo smrti", "zatratenie", "spása", "znovuzrodenie", "milosť"],
+    scriptureTheme: "Rimanom 3, 6, 10",
+    illustrationRef: "svetlo-cesta",
+    audioUrl: null,
+    hasAudio: false,
+    sections: [
+      {
+        type: "text",
+        content: "Drahý priateľ, drahá priateľka, vzácna duša, ktorá hľadá Pravdu,\n\nmožno práve prežívaš najťažšie obdobie svojho života. Tvoja myseľ je unavená, telo zoslabnuté a hľadáš odpovede na neriešiteľné situácie. Chcem ti však povedať jednu zásadnú pravdu: Nemôžeš vo svojom živote zakúsiť Božiu uzdravujúcu moc ani skutočné vedenie svojich krokov, kým neurobíš prvý, najdôležitejší krok.\n\nTým krokom je urovnanie tvojho vzťahu s nebeským Otcom cez Ježiša Krista – to, čo apoštol Pavel nazýva obnovením Svätým Duchom a novým stvorením. Bez tohto základu zostávaš odrezaný od zdroja a tvoj boj prebieha iba vo tvojej vlastnej ľudskej slabosti."
+      },
+      {
+        type: "verse",
+        text: "Veď všetci zhrešili a nemajú slávy Božej; ale ospravedlňovaní sú zadarmo z Jeho milosti, skrze vykúpenie v Kristovi Ježišovi.",
+        ref: "Rimanom 3, 23 – 24"
+      },
+      {
+        type: "text",
+        content: "Apoštol Pavel v liste Rimanom 6, 23 k tomu dodáva:"
+      },
+      {
+        type: "verse",
+        text: "Lebo odplatou za hriech je smrť, ale darom Božím z milosti je večný život v Kristovi Ježišovi, Pánovi našom.",
+        ref: "Rimanom 6, 23"
+      },
+      {
+        type: "text",
+        content: "Sami zo seba sme úplne neschopní dosiahnuť dokonalosť. Naše vlastné telo nás ťahá do hriechu, porážky a strachu zo smrti. Žiadne dobré skutky, ľudská psychológia ani vlastné úsilie nemôžu zmazať hriech a darovať nám istotu neba. Našťastie, Boh nenechal tvoju záchranu na tvojich pleciach. Ježiš Kristus vzal na kríž tvoje hriechy, tvoje slabosti i tvoj strach. On zaplatil celú cenu Svojou svätou krvou.\n\nAko sa táto Božia moc a zmena identity prijíma vo tvojom živote? Je to dar, ktorý prijímame čistou vierou. Písmo v liste Rimanom 10, 9 – 10 dáva jasné svedectvo:"
+      },
+      {
+        type: "verse",
+        text: "Ak ústami vyznávaš Pána Ježiša a v srdci veríš, že Ho Boh vzkriesil z mŕtvych, budeš spasený; lebo srdcom veríme na spravodlivosť a ústami vyznávame na spasenie.",
+        ref: "Rimanom 10, 9 – 10"
+      },
+      {
+        type: "text",
+        content: "V momente, keď toto urobíš, ťa Svätý Duch obnoví a v tvojom vnútri povstane úplne nové stvorenie. Tvoja myseľ síce môže byť stále nepokojná a telo unavené, no tvoj duch je v tej sekunde zapečatený Svätým Duchom a navždy spojený s Bohom. Dostávaš zasľúbenie večného života a moc Ducha Svätého, ktorá pôsobí v tvojom vnútri. Od tohto momentu už nie si obeťou strachu zo zatratenia ani otrokom hriechu. Si Božie dieťa, ktoré stojí pod ochranou nebeského Otca."
+      }
+    ],
+    prayer: "Drahý nebeský Otče, môj Stvoriteľ a môj Záchranca,\n\nprichádzam dnes pred Tvoju svätú tvár presne taký, aký som. Otváram pred Tebou celé svoje srdce a úprimne Ti vyznávam svoju obrovskú ľudskú slabosť. Priznávam, že som hriešny človek, neschopný zachrániť sám seba. Moja myseľ bola plná strachu zo smrti a budúcnosti, moje telo zažíva slabosť a moje vlastné úsilie ma doviedlo do slepej uličky. Odpusť mi, nebeský Otče, že som žil doteraz podľa svojich predstáv, bez Teba, a očisť ma od každého hriechu a nespravodlivosti.\n\nĎakujem Ti, Otče, že Tvoja moc sa dokonale prejavuje v tejto mojej slabosti. Verím Tvojmu Slovu, že si poslal Svojho Syna, Ježiša Krista, aby zomrel za moje zlyhania a na tretí deň vstal z mŕtvych pre moje ospravedlnenie. Ja dnes ústami vyznávam, že Ježiš Kristus je mojím Pánom a Spasiteľom. Vierou prijímam dar večného života a pravdu o tom, že ma robíš úplne novým stvorením. Staré veci pominuli a skrze Svätého Ducha sa všetko stalo novým.\n\nPane, nebudem Ťa pasívne prosiť, aby si ma prijal, pretože Tvoje Slovo sľubuje, že každý, kto vzýva meno Pánovo, bude spasený. Rozhodujem sa odpočívať v tejto absolútnej istote spasenia, ktorú mi daruješ z čistej milosti. Prijímam Svätého Ducha do svojho vnútra a otváram sa Tvojej premieňajúcej moci, ktorá uzdravuje moju dušu i telo. Od tejto chvíle dávam Tvojmu Slovu prvé miesto vo svojom živote a odmietam sa podriaďovať strachu.\n\nA preto teraz, ako znovuzrodené Božie dieťa, ktoré stojí v milosti, v mocnom mene Ježiša Krista vyhlasujem, že moc hriechu a večnej smrti nado mnou stratila všetku silu. Prikazujem každému duchu strachu, úzkosti, zatratenia a neistoty o mojej spáse: Umlknite a okamžite odíďte z mojej mysle i z môjho života!\n\nĎakujem Ti, Otče, už teraz – hoci ešte vlastnými očami nevidím východisko zo svojich ťažkostí. Moja dôvera sa neopiera o to, čo vidia moje oči, ale o Tvoju vernosť. Ja som slabý, ale Ten, ktorý odteraz žije vo mne, je všemohúci Víťaz.\n\nAmen."
+  },
+  {
+    id: "2",
+    title: "Sužujúce myšlienky, úzkosť a vnútorný chaos",
+    shortTitle: "Úzkosť a vnútorný chaos",
+    subtitle: "Ako mocou vyznaného Božieho Slova umlčať hlasy strachu a pochybností",
+    shortDescription: "Zbrane duchovného boja a moc vyznávania Slova nahlas.",
+    isStarter: false,
+    tags: ["úzkosť", "chaos", "myšlienky", "pokoj", "duchovný boj"],
+    scriptureTheme: "2. Korintským 10, 4 – 5; 1. Jánov 4, 4",
+    illustrationRef: "svetlo-pergamen",
+    audioUrl: null,
+    hasAudio: false,
+    sections: [
+      {
+        type: "text",
+        content: "Drahý brat, drahá sestra v Kristovi,\n\nv živote každého z nás prebieha zápas, ktorý sa odohráva predovšetkým v našej mysli. Možno práve teraz zažívaš dni, kedy je tvoje vnútro preťažené, myseľ unavená a tvoj každodenný život napĺňa úzkosť či chaos. Chcem, aby si vedel jednu zásadnú vec: tieto sužujúce myšlienky neurčujú to, kým v skutočnosti si.\n\nNepriateľ človeka, diabol, útočí najradšej potichu a nenápadne. Vnáša do tvojho vnútra pochybnosti a formuluje ich v prvej osobe, aby si si myslel, že sú to tvoje vlastné úvahy. Sú to tie známe hlasy: „Nie som dosť dobrý,“ „Moja situácia sa nikdy nezmení,“ alebo „Boh ma nepočuje.“\n\nAko človek obnovený Svätým Duchom a nové stvorenie v Kristovi však nie si proti tomuto tlaku bezbranný. Apoštol Pavel v 2. liste Korintským 10, 4 – 5 jasne definuje zbrane, ktoré nám Boh dal:"
+      },
+      {
+        type: "verse",
+        text: "...veď zbrane nášho boja nie sú telesné, ale od Boha majú moc zboriť hradby. Nimi búrame špekulácie a každú pýchu, čo sa dvíha proti poznaniu Boha, a každú myšlienku podrobujeme do poslušnosti Kristovej...",
+        ref: "2. Korintským 10, 4 – 5"
+      },
+      {
+        type: "text",
+        content: "Tento boj sa nevyhráva tak, že o týchto myšlienkach budeš potichu premýšľať alebo sa ich snažiť potlačiť vlastnou ľudskou vôľou. Útok na myseľ sa prerušuje vyznaním pravdy. Keď hovoríš nahlas Božie Slovo, tvoja myseľ sa musí sústrediť na to, čo vyjadrujú ústa.\n\nV tvojom duchu, ktorý bol zapečatený Duchom zasľúbenia, už dnes prebýva dokonalý Kristov pokoj. Tento pokoj nepotrebuješ v sebe zložito vyrábať ani ho hľadať vo svetskej psychológii. On už v tebe je. Hlasným vyznávaním Slova ho uvoľňuješ zo svojho vnútra, aby zaplavil tvoju nepokojnú dušu. Ako píše apoštol Ján v 1. Jánovom liste 4, 4:"
+      },
+      {
+        type: "verse",
+        text: "...väčší je Ten, ktorý je vo vás, ako ten, čo je vo svete.",
+        ref: "1. Jánov 4, 4"
+      },
+      {
+        type: "text",
+        content: "Postav sa dnes na toto svedectvo. Vzopri sa klamstvám nepriateľa a sleduj, ako pred mocou Božieho Slova musia ustúpiť."
+      }
+    ],
+    prayer: "Drahý nebeský Otče, Všemohúci Bože,\n\nprichádzam pred Tvoju svätú tvár v mene Tvojho Syna, Ježiša Krista. Otváram pred Tebou svoju myseľ, ktorá je unavená z neustáleho tlaku, úzkosti a sužujúcich myšlienok. Vyznávam, že niekedy podlieham strachu a verím klamstvám, že ma moja situácia zničí. Ty si mi však skrze Kristovu obetu odpustil moje zlyhania a urobil si ma novým stvorením, preto odmietam žiť v tichom trápení.\n\nĎakujem Ti, Otče, že v mojom duchu už prebýva dokonalý Kristov pokoj. Tento pokoj nezávisí od mojich pozemských okolností a moje myšlienky ho nemôžu zničiť. Tvoje Slovo ma uisťuje, že zbrane môjho boja majú moc zboriť každú hradbu strachu. Preto dnes beriem do rúk meč Ducha, ktorým je Tvoje živé Slovo.\n\nV mocnom mene Ježiša Krista sa staviam proti každému hlasu úzkosti, pochybností a strachu. Prehlasujem, že podľa listu Jakubovho 4, 7 sa podriaďujem Tebe, môj Bože, vzpieram sa diablovi a odmietam všetky sužujúce klamstvá v mojej mysli. Moja budúcnosť nie je v rukách úzkosti, ale v Tvojich milujúcich rukách.\n\nPodrobujem každú jednu myšlienku do poslušnosti Kristovi. Vyznávam, že som milovaný, prijatý a chránený Najvyšším Bohom. Tvoj Svätý Duch ma napĺňa silou, láskou a zdravou mysľou. Rozhodujem sa odpočívať v tejto pravde a nedovoliť svojim ústam hovoriť slová ustarostenosti a porážky.\n\nĎakujem Ti, nebeský Otče, že Tvoj neochvejný pokoj, ktorý prevyšuje každý ľudský rozum, teraz stráži moju myseľ i moje srdce v Kristovi Ježišovi. Ukončujem túto modlitbu s vedomím, že Ten, ktorý je vo mne, je väčší ako akýkoľvek strach.\n\nAmen."
+  },
+  {
+    id: "3",
+    title: "Hriech a odpustenie",
+    shortTitle: "Hriech a odpustenie",
+    subtitle: "Ako prijať Božie odpustenie a žiť v slobode",
+    shortDescription: "Pravda o očiste skrze Kristovu krv a sloboda od viny.",
+    isStarter: false,
+    tags: ["hriech", "odpustenie", "očista", "sloboda", "vina"],
+    scriptureTheme: "Žalm 51",
+    illustrationRef: "voda-svetlo",
+    audioUrl: null,
+    hasAudio: false,
+    sections: [
+      { type: "text", content: "(Text kapitoly pripravuje sa...)" },
+      { type: "verse", text: "(Verš pripravuje sa...)", ref: "Žalm 51" }
+    ],
+    prayer: "(Modlitba pripravuje sa...)"
+  },
+  {
+    id: "4",
+    title: "Samota a opustenosť",
+    shortTitle: "Samota a opustenosť",
+    subtitle: "Ako nájsť Božiu prítomnosť v osamelých chvíľach",
+    shortDescription: "Boh nikdy neopúšťa Svoje dieťa, ani v najtemnejšej noci.",
+    isStarter: false,
+    tags: ["samota", "opustenosť", "prítomnosť", "tma", "nádej"],
+    scriptureTheme: "Izaiáš 41",
+    illustrationRef: "hmla-svetlo",
+    audioUrl: null,
+    hasAudio: false,
+    sections: [
+      { type: "text", content: "(Text kapitoly pripravuje sa...)" },
+      { type: "verse", text: "(Verš pripravuje sa...)", ref: "Izaiáš 41" }
+    ],
+    prayer: "(Modlitba pripravuje sa...)"
+  },
+  {
+    id: "5",
+    title: "Smrť a smútok",
+    shortTitle: "Smrť a smútok",
+    subtitle: "Ako čeliť strate s nádejou vzkriesenia",
+    shortDescription: "Nádej, ktorá prevyšuje smútok – Kristus vzkriesil z mŕtvych.",
+    isStarter: false,
+    tags: ["smrť", "smútok", "nádej", "vzkriesenie", "strata"],
+    scriptureTheme: "1. Korintským 15",
+    illustrationRef: "svitanie",
+    audioUrl: null,
+    hasAudio: false,
+    sections: [
+      { type: "text", content: "(Text kapitoly pripravuje sa...)" },
+      { type: "verse", text: "(Verš pripravuje sa...)", ref: "1. Korintským 15" }
+    ],
+    prayer: "(Modlitba pripravuje sa...)"
+  }
+];
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = chapters;
+}
