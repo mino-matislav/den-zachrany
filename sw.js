7f1c87e85c52d39699b8f087e25592734aea1647
Den Zachrany Editor <editor@den-zachrany.local>
2026-07-27 19:18:49 +0000

    Piesne: názov piesne už nelezie pod navigáciu (horný odstup pre .song-page, platí pre všetky piesne); sw v8

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v7';
+const CACHE_NAME = 'den-zachrany-v8';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
