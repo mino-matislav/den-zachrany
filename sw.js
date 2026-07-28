13bc881bc0799bf9c0d7035c91d9ce99b4c5aa51
Den Zachrany Editor <editor@den-zachrany.local>
2026-07-28 18:27:29 +0000

    Scroll-náznak doplnený do úvodného slova (domov) a do šablóny kapitoly; sw v26

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v25';
+const CACHE_NAME = 'den-zachrany-v26';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
