50bd523dffbc564eb29d9b73024390b402e84e9a
Den Zachrany Editor <editor@den-zachrany.local>
2026-07-31 06:56:03 +0000

    Počúvaj: time/lišta vynútené na vlastný riadok (width:100%), Piesne: kompaktnejšia hlavička aby bolo vidno karty pod CTA; sw v54

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v53';
+const CACHE_NAME = 'den-zachrany-v54';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
