947706179bad48e4c04afb441a55d0f788d01b15
den-zachrany <den-zachrany@users.noreply.github.com>
2026-08-07 11:25:47 +0000

    Tlačidlo na stiahnutie vybraté z modrého poľa prehrávača: na PC je v hlavičke vpravo od nadpisu 🎵 (prehrávač si zachováva pôvodnú veľkosť), na mobile sa cez poradie flexu presunie pod prehrávač s rovnakým odstupom ako text vyššie; sw v108

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v107';
+const CACHE_NAME = 'den-zachrany-v108';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
@@ -12,7 +12,7 @@ const STATIC_ASSETS = [
     '/piesen.html',
     '/pocuvaj.html',
     '/podpora.html',
-    '/css/style.css?v=63',
+    '/css/style.css?v=64',
     '/js/app.js',
     '/js/player.js',
     '/js/data.js',
