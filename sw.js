af749d2511510dfa10a5726f7ea78caaf14f63ae
mino-matislav <mino.matislav@gmail.com>
2026-08-28 16:14:34 +0000

    Kapitola 22: nasadena audio modlitba (modlitba-22.mp3, 192k mono, -16.3 LUFS / -1.9 dBTP), hasAudio true; SW v147

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v146';
+const CACHE_NAME = 'den-zachrany-v147';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
