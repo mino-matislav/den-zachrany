8372e4487ba48d27a4fb595f80ed55ba4a869a4c
mino-matislav <mino.matislav@gmail.com>
2026-08-28 14:17:49 +0000

    Kapitola 22: prepracovany prihovor do stylu ostatnych kapitol (suvisla proza, 40 odsekov namiesto 140), preformulovana pasaz o identite; versе, modlitba a metadata bez zmeny; SW v146

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v145';
+const CACHE_NAME = 'den-zachrany-v146';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
