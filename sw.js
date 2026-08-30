0e3bca24e0bbfdc34449ae29e36c5fe08baf9923
mino-matislav <mino.matislav@gmail.com>
2026-08-30 19:57:12 +0000

    Piesen 19, sloha 2: vyrovnane koncove s v slove sytis na uroven kricis (+6 dB), zensky spev celej slohy 2 zosilneny o ~2.8 dB (priblizenie k slohe 1); cache-bust ?v=4; SW v156

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v155';
+const CACHE_NAME = 'den-zachrany-v156';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
