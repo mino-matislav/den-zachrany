8c6ffec2d7265865bde47404aaa9dab9477aa2c9
mino-matislav <mino.matislav@gmail.com>
2026-08-30 16:34:48 +0000

    SW v153 po reverte player.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v151';
+const CACHE_NAME = 'den-zachrany-v153';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
