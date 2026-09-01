3ae4b8e2c633eae3cb4fdaf53782f318fd03a44b
mino-matislav <mino.matislav@gmail.com>
2026-09-01 15:03:03 +0000

    Modlitba 22 re-master na profil vzorovych modlitieb 6/9: teplejsia (1-2k), jasnejsia v strede (2-3.5k clarity), meksie sykavky (5-8k stiahnute), prirodzenejsia dynamika; -16.5 LUFS/-1.7 dBTP; cache-bust ?v=3; SW v161

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v160';
+const CACHE_NAME = 'den-zachrany-v161';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
