2e314df77313f1591177c2e03398fe7270d5af62
mino-matislav <mino.matislav@gmail.com>
2026-09-02 08:41:34 +0000

    Modlitba (kap.) 1: pridane ~1s tiche intro pre odolnost pri stiahnuti/zdielani (Android prehravac ukusoval zaciatok, lebo modlitby zacinaju recou po 0.04s). Test pred rozsirenim na vsetky modlitby. Cache-bust ?v=6; SW v164

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v163';
+const CACHE_NAME = 'den-zachrany-v164';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
