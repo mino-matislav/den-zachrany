bd5bb7b51da413d42c4a20d5ff5a61b601b858e4
mino-matislav <mino.matislav@gmail.com>
2026-09-05 18:07:53 +0000

    Kapitola 24: nasadena audio modlitba (standard 6/7/8/9, buzz-fix + ~1s tiche intro, 192k mono -16.1 LUFS), hasAudio true. SW v176

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v175';
+const CACHE_NAME = 'den-zachrany-v176';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
