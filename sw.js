db20b996447b79bafbfbdff0a152e836b353ed7f
mino-matislav <mino.matislav@gmail.com>
2026-09-07 08:55:06 +0000

    Optimalizacia velkosti deploymentu (Vercel Hobby 10GB limit): odstranene osirotene subory - icon-192.png (bola to omylom 1.4MB MP3 nahravka s priponou .png!), icon-512.png (SVG s priponou .png), root data.js a data-kapitoly.js, js/data-kapitoly.js, stare uvod.mp3 a uvod-v2.mp3 (spolu 5.5 MB, nic sa na ne neodkazovalo - manifest pouziva assets/icon-*.png). Pridany .vercelignore (scripts, docs, README). SW v177

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v176';
+const CACHE_NAME = 'den-zachrany-v177';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
