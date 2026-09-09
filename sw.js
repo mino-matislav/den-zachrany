2bb2f0d20aa360673e792f6f2072d98b1e74cef8
mino-matislav <mino.matislav@gmail.com>
2026-09-09 17:17:39 +0000

    OPRAVA: kapitola 25 bola v data.js vlozena dvakrat (duplicitny blok), verify to zachytil - duplikat odstraneny. 25 blokov = 25 kapitol, verify 490 kontrol zeleny. SW v180

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v179';
+const CACHE_NAME = 'den-zachrany-v180';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
