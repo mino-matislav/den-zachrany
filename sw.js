ae0ece49989ae6d1dcbce7964eaabff2296572cb
mino-matislav <mino.matislav@gmail.com>
2026-09-10 18:40:28 +0000

    Uvodne slovo (uvod-v3): odstranene bzucanie - 5-8 kHz z -15.2 na -16.8 (modlitba 6 ma -17.1). Doplnene aj teplo (1-2k) a zrozumitelnost (2-3.5k), ktore boli 2.7-3.6 dB pod cielom. Format 48kHz stereo 160k zachovany, tiche intro OK, -16.4 LUFS. Cache-bust ?v=4. SW v184

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v183';
+const CACHE_NAME = 'den-zachrany-v184';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
