4ad2ea3e4a9cfe175056e7479fe06079a70c4cd2
mino-matislav <mino.matislav@gmail.com>
2026-09-10 19:12:31 +0000

    Uvodne slovo, druha polovica (od 54s): cielene stiahnute tri vycnievajuce useky. Meranie po 6s odhalilo, ze useky 54-60, 60-66 a 72-78s mali sykavky na -8.9 az -9.2 (zvysok -11.2) a buzz az -19.7. Po korekcii su vsetky na -11.1 az -12.7, rozptyl znizeny z 3.8 na 1.6 dB. Cache-bust ?v=7. SW v187

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v186';
+const CACHE_NAME = 'den-zachrany-v187';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
