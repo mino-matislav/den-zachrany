e61e20117e9b0783c694ff7020ae91d29585db5b
mino-matislav <mino.matislav@gmail.com>
2026-09-10 18:55:22 +0000

    Uvodne slovo: odstranene bzucanie v druhej polovici. Nahravka bola velmi nerovnomerna (rozptyl 9.2 dB medzi usekmi, najhorsi 70-80s na -12.0). Riesene dynamicky (kompresia HF pasma nad 5kHz) + cielenym cutom na vycnievajuce useky. Vysledok: celkovo -20.6 (medzi modlitbou 6 a 11), rozptyl znizeny na 4.7 dB, usek 70-80s zlepseny o 7.6 dB. Format 48kHz stereo zachovany. Cache-bust ?v=5. SW v185

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v184';
+const CACHE_NAME = 'den-zachrany-v185';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
