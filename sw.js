28e6f86603d35ca4723430f461b398ac6d223315
mino-matislav <mino.matislav@gmail.com>
2026-09-11 16:41:50 +0000

    Kapitola 26: nasadena audio modlitba podla NOVYCH pravidiel. Zdroj bol velmi tmavy (š/ž/č -22.9, buzz -24.2). Ciel podla referencii 5/6/7/11 s tmavsou stranou: buzz -20.7 (ciel -20), š/ž/č -12.6. Rozptyl po usekoch 4.4 dB (referencie maju 10-14, teda rovnomerne). Tiche intro 1.03s, 128k mono, -16.2 LUFS. SW v189

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v188';
+const CACHE_NAME = 'den-zachrany-v189';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
