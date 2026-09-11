0cae3ebd372f240cb9ebda4c5085e077df3489eb
mino-matislav <mino.matislav@gmail.com>
2026-09-11 16:49:45 +0000

    Modlitba 26: doladeny zaver (od 87s / 1:27). Meranie po 5s odhalilo v zavere nadbytok vo vysokom pasme 8-14 kHz (-16.3 az -16.7 oproti priemeru -18.1) a mierne aj buzz - odtial to bzucive sycanie. Cieleny cut 11k/8.5k/6.3k len na zaver: rozdiel voci zvysku znizeny z +2.0 na -0.9. Prva cast nedotknuta. Cache-bust ?v=2. SW v190

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v189';
+const CACHE_NAME = 'den-zachrany-v190';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
