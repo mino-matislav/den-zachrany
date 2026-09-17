a47c6fe047a4a3bfcce4722942f9d451a8431b68
Admin Rodacar <admin@den-zachrany.sk>
2026-09-17 09:38:08 +0000

    Domov: zjednotena sirka vsetkych blokov. Karty Uvodne slovo a Modlitba zachrany zo 680px na --content-max (720px), rovnako ako uvodny text a Dolezite upozornenie. Odstranene dvojite bocne odsadenie sekcii (sekcia + container) - na mobile mali bloky rozne sirky. Mobilne odsadenie intro-content zjednotene s ostatnymi kartami. SW v196.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DEŇ ZÁCHRANY - Service Worker
    ============================================ */
 
-const CACHE_NAME = 'den-zachrany-v195';
+const CACHE_NAME = 'den-zachrany-v196';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
